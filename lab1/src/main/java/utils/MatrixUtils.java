ed47dac8d113a4b344dd5d7aff48ef4bd7ef8afb
Zachary Bell <zacharybell@live.com>
2018-01-28 21:59:46 -0700

    Accuracy Function
    Computes accuracy between predictions and labels.

--- lab1/src/main/java/utils/MatrixUtils.java
+++ lab1/src/main/java/utils/MatrixUtils.java
@@ -2,12 +2,33 @@ package utils;
 
 import org.apache.commons.math3.linear.Array2DRowRealMatrix;
 import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.linear.RealVector;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 import java.util.Random;
 
 public class MatrixUtils {
-    public static RealMatrix shuffle(RealMatrix matrix, int seed) {
+
+    public static double accuracy(@NotNull RealVector AVector, @NotNull RealVector BVector) {
+        double epsilon = 0.000001;
+
+        double[] A = AVector.toArray();
+        double[] B = BVector.toArray();
+
+        if (A.length != B.length) throw new IllegalArgumentException("Both vectors need to be of the same length!");
+        if (A.length == 0) throw new IllegalArgumentException("Vectors cannot be of length 0!");
+
+        double error = 0;
+
+        for (int i = 0; i < A.length; i++) {
+            if (Math.abs(Double.compare(A[i], B[i])) < epsilon) error += 1;
+        }
+
+        return error / A.length;
+    }
+
+    public static RealMatrix shuffle(@NotNull RealMatrix matrix, int seed) {
         Random random = new Random(seed);
 
         double[][] data = matrix.getData();
@@ -19,7 +40,7 @@ public class MatrixUtils {
         return new Array2DRowRealMatrix(data);
     }
 
-    public static RealMatrix normalize(RealMatrix matrix) {
+    public static RealMatrix normalize(@NotNull RealMatrix matrix) {
         double[][] data = matrix.transpose().getData();
 
         for (double[] e : data) {
@@ -29,7 +50,7 @@ public class MatrixUtils {
         return (new Array2DRowRealMatrix(data)).transpose();
     }
 
-    public static void normalize(double[] vector) {
+    public static void normalize(@NotNull double[] vector) {
         double max = Double.MIN_VALUE;
         double min = Double.MAX_VALUE;
 
@@ -47,7 +68,7 @@ public class MatrixUtils {
         }
     }
 
-    private static void swap(double[][] array, int i, int j) {
+    private static void swap(@NotNull double[][] array, int i, int j) {
         double[] temp;
         temp = array[i];
         array[i] = array[j];
