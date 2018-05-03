8a43c513a51d135d8f0306bf04f93b19cc58f4d2
zacharybell <zacharybell@live.com>
2018-05-03 04:35:03 -0600

    Add reverse_image function which mirrors images for data augmentation

--- lab6/face-classifier/preprocessing.py
+++ lab6/face-classifier/preprocessing.py
@@ -18,6 +18,25 @@ def load_lfw_data(path=TARGET_PATH, mfpp=0):
     lfw_labels                 = pd.DataFrame()
     lfw_labels['target_names'] = pd.Series(target_names)
     lfw_labels['target_id']    = pd.Series(lfw_bunch['target'])
-    lfw_images                 = lfw_bunch['data']
+    lfw_images                 = lfw_bunch['data'] / 255
 
-    return lfw_images, lfw_labels
+    dim = lfw_bunch['images'].shape
+
+    return lfw_images, lfw_labels, dim
+
+def reverse_images(images: np.ndarray, dim: tuple) -> np.ndarray:
+    images = images.reshape((-1, dim[0], dim[1]))
+    images = np.flip(images, axis=2).reshape((-1,dim[0] * dim[1]))
+    return images
+
+
+## reverse_images testing
+# i1 = np.array([[1, 2, 3], [4, 5, 6]])
+# i2 = np.array([[10, 20, 30], [40, 50, 60]])
+#
+# i1 = i1.flatten()
+# i2 = i2.flatten()
+#
+# images = np.array([i1, i2])
+# print(images.shape)
+# print(reverse_images(images, (2, 3)))
