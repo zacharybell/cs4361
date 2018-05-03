40785bbcd4a63065f58ce1c4c8fd7ee96371a810
zacharybell <zacharybell@live.com>
2018-05-03 04:05:51 -0600

    Add function for loading data into features and labels

--- lab6/face-classifier/preprocessing.py
+++ lab6/face-classifier/preprocessing.py
@@ -11,9 +11,13 @@ def convert_id_to_name(idx, names_idx):
         names[i] = names[idx[i]]
     return names
 
-lfw_bunch                  = fetch_lfw_people(data_home=TARGET_PATH)
-target_names               = convert_id_to_name(lfw_bunch['target'], lfw_bunch['target_names'])
-lfw_labels                 = pd.DataFrame()
-lfw_labels['target_names'] = pd.Series(target_names)
-lfw_labels['target_id']    = pd.Series(lfw_bunch['target'])
-lfw_images                 = lfw_bunch['data']
+def load_lfw_data(path=TARGET_PATH, mfpp=0):
+    lfw_bunch                  = fetch_lfw_people(data_home=TARGET_PATH,
+                                                  min_faces_per_person=mfpp)
+    target_names               = convert_id_to_name(lfw_bunch['target'], lfw_bunch['target_names'])
+    lfw_labels                 = pd.DataFrame()
+    lfw_labels['target_names'] = pd.Series(target_names)
+    lfw_labels['target_id']    = pd.Series(lfw_bunch['target'])
+    lfw_images                 = lfw_bunch['data']
+
+    return lfw_images, lfw_labels
